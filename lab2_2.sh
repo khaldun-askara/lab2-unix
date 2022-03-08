4027f29adb1a1ecef6fd5b7ba2e2307a7839877f
Khaldun Askara <mkurotsuchi0@gmail.com>
2022-03-08 21:57:48 +0500

    boop

--- lab2_2.sh
+++ lab2_2.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+function get_variants {
+    if [ ${sticks} -ge 4 ]
+    then
+        return "1 2 3 4"
+    elif [ ${sticks} -eq 3 ]
+    then 
+        return "1 2 3"
+    elif [ ${sticks} -eq 2 ]
+    then
+        return "1 2"
+    elif [ ${sticks} -eq 1 ]
+    then
+        return "1"
+    else
+        return ""
+}
+
 echo "    /\_____/\\"
 echo "   /  o   o  \       HI!!"
 echo "  ( ==  ^  == )"
@@ -10,3 +27,6 @@ echo "(__(__)___(__)__)"
 echo "Введите количество палочек!"
 read sticks
 echo "Введено палочек: ${sticks}"
+str = get_variants
+echo "${str}"
+

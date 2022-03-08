7a04bccbae8ee26839f8e61ee364f6fc12cd66fd
Khaldun Askara <mkurotsuchi0@gmail.com>
2022-03-08 22:14:26 +0500

    boop

--- lab2_2.sh
+++ lab2_2.sh
@@ -31,3 +31,29 @@ get_variants
 str=$variants
 echo "${str}"
 
+while [ ${sticks} -ge 1 ]
+do
+    echo "Новая итерация, ${sticks}"
+    # Пользовательское меню
+    echo "Выберите, сколько хотите взять палочек!"
+    get_variants
+    OPTIONS=$variants
+    select opt in ${OPTIONS}
+    do
+        if [ ${opt} = "1" ]
+        then
+            sticks=${sticks}-1
+        elif [ ${opt} = "2" ]
+        then
+            sticks=${sticks}-2
+        elif [ ${opt} = "3" ]
+        then
+            sticks=${sticks}-3
+        elif [ ${opt} = "4" ]
+        then
+            sticks=${sticks}-4
+        else
+            echo "Bad option"
+        fi
+    done
+done

b80575100578beba4c3cf67c6238b71846db4145
Khaldun Askara <mkurotsuchi0@gmail.com>
2022-03-08 20:35:52 +0500

    added kitten to task 2

--- lab2_2.sh
+++ lab2_2.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo "    /\_____/\\"
+echo "   /  o   o  \       HI!!"
+echo "  ( ==  ^  == )"
+echo "   )         ("
+echo "  (           )"
+echo " ( (  )   (  ) )"
+echo "(__(__)___(__)__)"
+echo "Введите количество палочек!"
+read sticks
+echo "Введено палочек: ${sticks}"

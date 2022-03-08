4a647435ea276593b2f85f3f7c7b4db6fa755299
Khaldun Askara <mkurotsuchi0@gmail.com>
2022-03-08 17:06:47 +0000

    bruh

--- lab2_2.sh
+++ lab2_2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 function get_variants {
+    variants=""
     if [ ${sticks} -ge 4 ]
     then
-        return "1 2 3 4"
+        variants="1 2 3 4"
     elif [ ${sticks} -eq 3 ]
     then 
-        return "1 2 3"
+        variants="1 2 3"
     elif [ ${sticks} -eq 2 ]
     then
-        return "1 2"
+        variants="1 2"
     elif [ ${sticks} -eq 1 ]
     then
-        return "1"
-    else
-        return ""
+        variants="1"
+    fi
 }
 
 echo "    /\_____/\\"
@@ -27,6 +27,7 @@ echo "(__(__)___(__)__)"
 echo "Введите количество палочек!"
 read sticks
 echo "Введено палочек: ${sticks}"
-str = get_variants
+get_variants
+str=$variants
 echo "${str}"
 
